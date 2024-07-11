5f1bb906c673871da2341f84ea3568b2ec80bbaf
Adonys Santos <58638286+adonyssantos@users.noreply.github.com>
2024-07-10 21:05:32 -0400

    add mega.nz uploading validation to the create-backups script

--- scripts/create-backup.sh
+++ scripts/create-backup.sh
@@ -1,7 +1,9 @@
 # This scripts sets up the backups for all the volumes in the system
-# The backups are compress, encrypted and stored in Mega.nz cloud storage
+# The backups are compress, encrypted
+# Also, if the day is Monday or upload-to-mega is true, the backup is uploaded to Mega.nz
 
 timestamp=$(date +"%Y%m%d%H%M%S")
+upload_to_mega=$1
 
 # Create a temporary directory to store the backup
 rm -rf ../.temp
@@ -17,7 +19,7 @@ gpg --output ../backups/$timestamp.tar.gz.gpg --symmetric --cipher-algo AES256 -
 # Clean up the temporary directory
 rm -rf ../.temp
 
-# TODO: Upload the backup to Mega.nz on Mondays
-if [ $(date +%u) -eq 1 ]; then
+# TODO: Upload the backup to Mega.nz on Mondays or if upload_to_mega is true
+if [ $(date +%u) -eq 1 ] || [ "$upload_to_mega" == "true" ]; then
     # TODO: Upload the backup to Mega.nz
 fi
