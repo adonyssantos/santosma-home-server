e5585fdbf97981256a000044ca8ef727bc99fc94
Adonys Santos <58638286+adonyssantos@users.noreply.github.com>
2024-08-04 22:07:11 -0400

    fix clean-backups script to remove backups older than 30 days

--- scripts/clean-backups.sh
+++ scripts/clean-backups.sh
@@ -7,17 +7,21 @@ if [ -d "./scripts" ]; then
 fi
 
 BASE_DIR=${1:-$(pwd)}
-current_timestamp=$(date +"%Y%m%d%H%M%S")
+current_date=$(date +%s)
 
 cd $BASE_DIR
 
 for backup in ../backups/*.tar.gz.gpg; do
-    timestamp=$(basename $backup .tar.gz.gpg)
-    
-    difference_in_days=$(( (current_timestamp - timestamp) / 86400 ))
+  backup_timestamp=$(basename $backup .tar.gz.gpg)
+  backup_date=${backup_timestamp:0:8}
+  backup_date_seconds=$(date -d $backup_date +%s)
+  difference_in_days=$(( (current_date - backup_date_seconds) / 86400 ))
 
-    if [ $difference_in_days -gt 30 ]; then
-        rm $backup
-        echo "Backup $backup removed."
-    fi
+  # Remove the backup if it is older than 30 days
+  if [ $difference_in_days -gt 30 ]; then
+    echo "Removing backup $backup"
+    rm $backup
+  else
+    echo "Backup $backup is not older than 30 days"
+  fi
 done
